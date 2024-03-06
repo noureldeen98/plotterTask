d28def0a38d181ef4f96c052f3cec89868b79ee0
Nour-Eldeen <noureldeen.nasser@tmentors.com>
2024-03-06 23:54:26 +0200

    Listing the columns data and each element can be dragged

--- src/components/layout/Columns.tsx
+++ src/components/layout/Columns.tsx
@@ -1,9 +1,28 @@
-import React, { useState } from 'react'
+import axios, { AxiosResponse } from 'axios';
+import { useEffect, useState } from 'react'
+import { Column } from '../utils/models/column';
+import Draggable from 'react-draggable';
 
 const Columns = () => {
+  const baseURL="https://plotter-task-8019e13a60ac.herokuapp.com/columns"
+  const [columns, setColumns] = useState([]);
+  //Side effect : Getting the columns 
+  useEffect(()=>{
+    axios.get(`${baseURL}`).then((response:AxiosResponse)=>{
+      setColumns(response.data.columns)
+    })
+  },[])
   return (
-    <div>
-      
+    <div className='w-[100%] text-start h-[690px]'>
+      <p className='mt-5'>Columns</p>
+      <hr className='border-[2px] border-[lightGray]'/>
+       <ul className='gap-4 flex flex-col items-start mt-4'>
+        {columns.map((column:Column)=>(
+          <Draggable>
+            <li>{column.name}</li>
+          </Draggable>
+        ))}
+       </ul>
     </div>
   )
 }
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className='w-[100%] text-center  h-[100px]'>
+      <p>Plotter Chart</p>
     </div>
   )
 }
