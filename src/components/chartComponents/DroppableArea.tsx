261b4854ba61d4ec030d1bad168f987a67ceb834
Nour-Eldeen <noureldeen.nasser@tmentors.com>
2024-03-09 05:11:17 +0200

    Enhancements in UI

--- src/components/chartComponents/DroppableArea.tsx
+++ src/components/chartComponents/DroppableArea.tsx
@@ -5,9 +5,8 @@ import { Column } from "../utils/models/column";
 import LineChart from "./LinearChart";
 import { handleClearDimension } from "../state/slices/DragandDropSlice";
 
-
 const DroppableArea = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const [chartData, setChartData] = useState([{ name: "", values: [] }]);
   const baseURL = "https://plotter-task-8019e13a60ac.herokuapp.com/data";
   const measureAreaRef = useRef(undefined);
@@ -44,43 +43,39 @@ const DroppableArea = () => {
   }, [dimesnsionElement, measuresArray]);
 
   //Clearing the Dimension field
-  const handleDimensionClear = ()=>{
-    dispatch(handleClearDimension())
-  }
+  const handleDimensionClear = () => {
+    dispatch(handleClearDimension());
+  };
 
   return (
     <>
-      <div>
-        <div className="flex flex-col gap-2 justify-center">
-          <div className="flex flex-row gap-2">
-            <label>Dimensions</label>
-            <div
-              ref={dimensionAreaRef}
-              className="border-solid w-[500px] h-[100px] border-black border-2"
-              id="droppable-dimension-area"
-            ></div>
-            <button onClick={handleDimensionClear}>Clear</button>
-          </div>
-
-          <div className="flex flex-row gap-2">
-            <label>Measures</label>
-            <div
-              ref={measureAreaRef}
-              className="border-solid w-[500px] h-[100px] border-black border-2"
-              id="droppable-measure-area"
-            >
-            </div>
-          </div>
+      <div className="flex flex-col gap-2 justify-center items-center">
+        <div className="flex flex-row gap-2 items-center justify-between">
+          <label>Dimensions</label>
+          <div
+            ref={dimensionAreaRef}
+            className="border-solid w-[500px] h-[50px] border-black border-2"
+            id="droppable-dimension-area"
+          ></div>
+          <button onClick={handleDimensionClear}>Clear</button>
+        </div>
+        <div className="flex flex-row gap-2 items-center justify-between">
+          <label className="pr-3">Measures</label>
+          <div
+            ref={measureAreaRef}
+            className="border-solid w-[500px] h-[50px] border-black border-2"
+            id="droppable-measure-area"
+          ></div>
           <button>Clear</button>
         </div>
       </div>
-      <div>
-        <LineChart chartOptions={chartData} />
-      </div>
+      {chartData[0].name !== "" && (
+        <div>
+          <LineChart chartOptions={chartData} />
+        </div>
+      )}
     </>
   );
 };
 
 export default DroppableArea;
-
-
